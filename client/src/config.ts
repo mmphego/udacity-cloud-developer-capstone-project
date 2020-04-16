f2bf4bf13679d7b8a9743b177e871ec91f6ad755
Mpho Mphego <mpho112@gmail.com>
2020-04-16 06:44:02 +0200

    Added daily diary client
    React based client for serverless daily diary based on: https://github.com/mmphego/cloud-developer/tree/master/course-04/project/c4-final-project-starter-code

---
+++ client/src/config.ts
@@ -0,0 +1,9 @@
+const apiId = 'xa69eap5s3'
+const region = 'us-east-1'
+export const apiEndpoint = `https://${apiId}.execute-api.${region}.amazonaws.com/dev`
+
+export const authConfig = {
+  domain: 'dev-hhrq1tik.auth0.com',
+  clientId: 'zv2uxvDOZNicGc1gI7dMGTE8Ad30fCEI',
+  callbackUrl: 'http://localhost:3000/callback'
+}
---
+++ client/src/types/Todo.ts
@@ -0,0 +1,9 @@
+export interface Todo {
+  todoId: string
+  createdAt: string
+  name: string
+  dueDate: string
+  done: boolean
+  attachmentUrl?: string
+  validUrl?: boolean
+}
