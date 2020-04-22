d3dad6224c19cc94990e52fba3ac5aeb3cb7f1a9
Mpho Mphego <mpho112@gmail.com>
2020-04-22 21:35:12 +0200

    added authentication via auth0
    Signed-off-by: Mpho Mphego <mpho112@gmail.com>

---
+++ backend/src/auth/utils.ts
@@ -0,0 +1,33 @@
+import { decode } from 'jsonwebtoken'
+import { JwtPayload } from './JwtPayload'
+
+/**
+ * Parse a JWT token and return a user id
+ *
+ * @param jwtToken JWT token to parse
+ *
+ * @returns a user id from the JWT token
+ */
+export function parseUserId(jwtToken: string): string {
+  const decodedJwt = decode(jwtToken) as JwtPayload
+  return decodedJwt.sub
+}
+
+/**
+ * Retrieve the token from the authorization header
+ *
+ * @param authHeader Authorization header
+ *
+ * @returns a token from the authorization header
+ */
+export const getToken = (authHeader: string): string => {
+  if (!authHeader) throw new Error('No authentication header')
+
+  if (!authHeader.toLowerCase().startsWith('bearer '))
+    throw new Error('Invalid authentication header')
+
+  const split = authHeader.split(' ')
+  const token = split[1]
+
+  return token
+}
