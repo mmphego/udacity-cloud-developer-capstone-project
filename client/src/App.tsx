356aad0ecfffdab74c6b294556c4416305d1a866
Mpho Mphego <mpho112@gmail.com>
2020-04-24 12:12:45 +0200

    Updated App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,10 +2,10 @@ import React, { Component } from 'react'
 import { Link, Route, Router, Switch } from 'react-router-dom'
 import { Grid, Menu, Segment } from 'semantic-ui-react'
 import Auth from './auth/Auth'
-import { EditTodo } from './components/EditTodo'
+import { EditDiary } from './components/EditDiary'
 import { LogIn } from './components/LogIn'
 import { NotFound } from './components/NotFound'
-import { Todos } from './components/Todos'
+import { Diaries } from './components/Diaries'
 
 export interface AppProps {}
 
@@ -91,15 +91,15 @@ export default class App extends Component<AppProps, AppState> {
           path="/"
           exact
           render={(props) => {
-            return <Todos {...props} auth={this.props.auth} />
+            return <Diaries {...props} auth={this.props.auth} />
           }}
         />
 
         <Route
-          path="/todos/:todoId/edit"
+          path="/diaries/:diaryId/edit"
           exact
           render={(props) => {
-            return <EditTodo {...props} auth={this.props.auth} />
+            return <EditDiary {...props} auth={this.props.auth} />
           }}
         />
 
